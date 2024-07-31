d89f0cbd8028172ba3f9518d31a42bcc503a7e90
Jam1eL1 <23jisu@gmail.com>
2024-07-31 13:45:40 -0500

    feat: add game functions and initialize scores
    - Implement `getComputerChoice` function to return a random choice between rock, paper and scissors.
    - Implement `getHumanChoice` function to prompt user input, normalize it and validate against allowed choices.
    - Initialize `humanScore` and `computerScore`.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random()*choices.length);
+    return choices[randomIndex];
+}
+
+let humanScore = 0, computerScore = 0;
+
+function getHumanChoice() {
+    let humanChoice = prompt('Enter your choice');
+    humanChoice = humanChoice ? humanChoice.trim().toLowerCase() : "";
+    const validChoices = ['rock', 'paper', 'scissors'];
+    if (validChoices.includes(humanChoice)) {
+        return humanChoice;
+    } else {
+        return null;
+    }
+}
+
