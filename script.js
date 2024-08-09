99faa7001a2a7f25422f3a0f705884097666b7c1
Jam1eL1 <23jisu@gmail.com>
2024-08-09 12:47:09 -0500

    feat: implement game logic and score tracking
    - Create global variables for computer and human scores.
    - Implement playRound function to manage game outcomes.
    - Create a dictionary variable `result` to determine the outcome of each round.
    - Add loop to play 6 rounds of the game and display results.

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ function getComputerChoice() {
     return choices[randomIndex];
 }
 
-let humanScore = 0, computerScore = 0;
-
 function getHumanChoice() {
     let humanChoice = prompt('Enter your choice');
     humanChoice = humanChoice ? humanChoice.trim().toLowerCase() : "";
@@ -17,3 +15,33 @@ function getHumanChoice() {
     }
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    const result = {"rock": {"rock": "draw", "paper": "lose", "scissors": "win"},
+                    "paper": {"rock": "win", "paper": "draw", "scissors": "lose"},
+                    "scissors": {"rock": "lose", "paper": "win", "scissors": "draw"}}
+
+    if (result[humanChoice][computerChoice] === 'win') {
+        console.log(`You win, ${humanChoice} beats ${computerChoice}. `);
+        humanScore++;
+    } else if (result[humanChoice][computerChoice] === 'draw') {
+        console.log(`You draw, you both chosen ${humanChoice}. `);
+        humanScore++;
+        computerScore++;
+    } else {
+        console.log(`You lose, ${computerChoice} beats ${humanChoice}.`);
+        computerScore++;
+    }
+    console.log(`Scores: You: [${humanScore}] vs Computer: [${computerScore}]`);
+}
+
+
+
+for (let i=0; i < 6; i++) {
+    console.log(`-------Game round ${i}------`);
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+}
